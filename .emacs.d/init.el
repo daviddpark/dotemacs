88dd3dd1144f89ca2ea157323ba43bee6859b27f
David Park <daviddpark@gmail.com>
2012-09-21 09:38:56 -0400

    moved some directories around for portability

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,12 +17,17 @@
 (load-theme 'tango-dark)
 
 ;; Load and configure SLIME
-(add-to-list 'load-path "/opt/slime")
+(add-to-list 'load-path "slime")
 (require 'slime)
 (eval-after-load 'slime '(setq slime-protocol-version 'ignore))
 (slime-setup '(slime-repl))
 
 ;; Load a major mode for editing Clojure code.
-(add-to-list 'load-path "/opt/clojure-mode")
+(add-to-list 'load-path "clojure-mode")
 (require 'clojure-mode)
   (require 'clojure-test-mode) ;; requires slime
+
+(setq org-directory "~/orgmode")
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
+(setq org-mobile-inbox-for-pull "~/orgmode/orgmobileinbox.org")
+
